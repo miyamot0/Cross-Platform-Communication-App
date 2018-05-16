32fc4d1f243eb7b30bf2600ed98b531a158baddc
Shawn Gilroy <shawn.gilroy@temple.edu>
2018-05-16 16:52:25 +0100

    Change OG License from GPL 3.0+ to MPL 2.0

--- SGDWithCocos/SGDWithCocos.Shared/App.cs
+++ SGDWithCocos/SGDWithCocos.Shared/App.cs
@@ -2,19 +2,11 @@
 // <copyright file="App.cs" 
 // Copyright August 18, 2016 Shawn Gilroy
 //
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/. 
 // This file is part of Cross Platform Communication App
 //
-// Cross Platform Communication App is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, version 3.
-//
-// Cross Platform Communication App is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Cross Platform Communication App.  If not, see http://www.gnu.org/licenses/. 
 // </copyright>
 //
 // <summary>
